89f1a21b4dac507af3bf9f93f73238a8d5b3eb4b
Pazhiloi <martinsen06@gmail.com>
2022-02-11 13:15:40 +0200

    first commit

---
+++ src/Table.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Table.css";
+import numeral from "numeral";
+const Table = ({ countries }) => {
+  return (
+    <div className="table">
+      {countries.map((country) => (
+        <tr key={country.country}>
+          <td>{country.country}</td>
+          <td>
+            <strong>{numeral(country.cases).format("0,0")}</strong>
+          </td>
+        </tr>
+      ))}
+    </div>
+  );
+};
+
+export default Table;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCd5gB6MR-yGg1LqGpQiCaWcNW3ZppO3j4",
+  authDomain: "cov-19-tracker-b05c5.firebaseapp.com",
+  projectId: "cov-19-tracker-b05c5",
+  storageBucket: "cov-19-tracker-b05c5.appspot.com",
+  messagingSenderId: "367430671065",
+  appId: "1:367430671065:web:aceb42ef0d07e3fb26bcc8",
+};
